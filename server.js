4c032203c3baf7a4a9ad3ff8b25ec9b8145e8144
lusoner <luxuan1023@gmail.com>
2017-01-22 17:35:19 +0800

    test mongo

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var http = require('http');
+var mongodb = require('mongodb');
+
+
+
 var serv = http.createServer(function(req, res){
 	res.writeHead(200,{'Content-Type':'text/html'});
 	res.end('<marquee>Smashing Node!</marquee>');
+
+	var server = new mongodb.Server("www.fanhaobai.com",27017,{});//本地27017端口
+
+	new mongodb.Db('mongotest',server,{}).open(function(error,client){//数据库：mongotest
+	    if(error) throw error;
+		client.collection(table, function(err,collection) {
+			collection.find(function(error,cursor){
+				callback(error,cursor);
+			});
+		});
+	});
 })
 serv.listen(8000);
