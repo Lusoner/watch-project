69ccd48d6d4aa047ba0764388d0951ad2575f4a7
Lusoner <luxuan1023@gmail.com>
2017-01-22 15:55:51 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 console.log(1111);
 onmessage = function(data){
 	console.log(data);
-	postMessage(data+',return a new message return a new message'');
+	postMessage(data+',return a new message return a new message');
 }
